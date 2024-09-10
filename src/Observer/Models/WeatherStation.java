53c0d7a527e14907f23ca5a05c779e42033db69c
Tatu <tatu.sailynoja@gmail.com>
2024-09-10 15:59:07 +0300

    finalized

--- src/Observer/Models/WeatherStation.java
+++ src/Observer/Models/WeatherStation.java
@@ -4,28 +4,58 @@ import Observer.Interfaces.ObserverInterface;
 import Observer.Interfaces.PublisherInterface;
 import java.util.ArrayList;
 
-public class WeatherStation implements PublisherInterface {
-    private ArrayList<ObserverInterface> observers;
+public class WeatherStation implements PublisherInterface, Runnable {
+    private final ArrayList<ObserverInterface> observers;
+    private boolean running;
     private int temperature;
-    
+
     public WeatherStation() {
+        updateTemperature();
         observers = new ArrayList<>();
+        running = true;
     }
 
     @Override
+    public void run() {
+        while (running) {
+            updateTemperature();
+            notifyObservers();
+            try {
+                // Sleep for between 1 and 3 seconds.
+                int sleepTime = (int) (Math.random() * 2000)+1000;
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+
+
+
+    @Override
     public void addObserver(ObserverInterface observer) {
+        System.out.println("Adding observer: " + observer.getName());
         observers.add(observer);
     }
 
     @Override
     public void removeObserver(ObserverInterface observer) {
+        System.out.println("Removing observer: " + observer.getName());
         observers.remove(observer);
     }
 
     @Override
     public void notifyObservers() {
+        String message = "Temperature is " + this.getTemperature();
+        System.out.println("Notifying observers: ");
         for (ObserverInterface observer : observers) {
-            observer.update();
+            observer.update(message);
         }
     }
 
